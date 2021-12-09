7fb362f23e46f6bf3360f1c4ed38326d0f58aeae
Your Name <you@example.com>
2021-12-09 12:27:36 +0100

    Prüfung m411 2

---
+++ Prüfung_M411_2/Prüfung_M411.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void ConvertDecimal2Binary(unsigned long dec, unsigned char bin[], unsigned char* pLength);
+
+int main()
+{
+  // Decimal 2 Binary
+  // Enea K. 2021
+
+  unsigned long num;
+  unsigned char binary_num[64];
+  unsigned char len = 3;
+
+  printf("Dieses Programm konvertiert eine Dezimalzahl in die Dualzahl ...\n");
+  printf("Geben Sie eine positive Dezimalzahl ein >");
+  scanf_s("%d", &num);
+
+  ConvertDecimal2Binary(num, binary_num, &len);
+
+  printf("Die Dualzahl>");
+  int padding = 0;
+  for (int j = len - 1; j >= 0; j--) {
+    if (padding == 7) {
+      printf("%d ", binary_num[j]);
+      padding = 0;
+    }
+    else {
+      printf("%d", binary_num[j]);
+      padding++;
+    }
+
+  }
+}
+
+void ConvertDecimal2Binary(unsigned long dec, unsigned char bin[], unsigned char* pLength) {
+  int i = 0;
+  while (dec > 0)
+  {
+    bin[i] = dec % 2;
+    dec = dec / 2;
+    i++;
+  }
+  *pLength = i;
+};
