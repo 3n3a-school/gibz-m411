794c5b5c52befc49f74349c96698b8c1c1706111
3n3a <info@3n3a.ch>
2021-12-20 11:45:08 +0100

    Fully funcitonal new version

--- Adressbuc/addrb.c
+++ Adressbuc/addrb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sqlite3.h>
 
@@ -7,6 +8,12 @@ typedef struct {
 	int number;
 	char name[20];
 } menuitem;
+
+typedef struct {
+	char name[20];
+	char addr[50];
+	char phone[10];
+} contact;
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 	int i;
@@ -30,7 +37,7 @@ void create_tables(sqlite3 *db, char *zErrMsg) {
 	int rc;
 	char *sql_create_table;
 	sql_create_table = "CREATE TABLE CONTACTS(" \
-			    "ID INT PRIMARY KEY	NOT NULL,"\
+			    "ID INTEGER PRIMARY KEY AUTOINCREMENT,"\
 			    "NAME	TEXT	NOT NULL,"\
 			    "ADDRESS	CHAR(50),"\
 			    "PHONENUMBER CHAR(10));";
@@ -44,20 +51,38 @@ void create_tables(sqlite3 *db, char *zErrMsg) {
 	}
 }
 
-void insert_records(sqlite3 *db, char *zErrMsg) {	
+void insert_records(sqlite3 *db, char *zErrMsg, contact cnt) {	
 	int rc;
 	char *insert_init;
-	insert_init = "INSERT INTO CONTACTS (ID,NAME,ADDRESS,PHONENUMBER) " \
-		       "VALUES (1, '3n3a', 'Street 1', '0791234567');";
+	sqlite3_stmt *stmt;
+	insert_init = "INSERT INTO CONTACTS (NAME,ADDRESS,PHONENUMBER) " \
+		       "VALUES (?, ?, ?);";
 
-	rc = sqlite3_exec(db, insert_init, callback, 0, &zErrMsg);
+	rc = sqlite3_prepare_v2(db, insert_init, -1, &stmt, NULL);
 
 	if ( rc != SQLITE_OK ){
 		fprintf(stderr, "SQL Error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
-	} else {
-		fprintf(stdout, "Record inserted with success!\n");
 	}
+
+	sqlite3_bind_text(stmt, 1, cnt.name, 20, SQLITE_TRANSIENT);
+	sqlite3_bind_text(stmt, 2, cnt.addr, 50, SQLITE_TRANSIENT);
+	sqlite3_bind_text(stmt, 3, cnt.phone, 10, SQLITE_TRANSIENT);
+	
+	for (;;) {
+		rc = sqlite3_step(stmt);
+		if (rc != SQLITE_ROW)
+			break;	
+	}
+
+	if ( rc != SQLITE_DONE ) {
+		fprintf(stderr, "SQL Execution failed: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(stmt);
+		return;
+	}
+
+	sqlite3_finalize(stmt);
+	return;
 }
 
 void select_all_records(sqlite3 *db, char *zErrMsg) {	
@@ -74,28 +99,71 @@ void select_all_records(sqlite3 *db, char *zErrMsg) {
 	}
 }
 
+
+void delete_record(sqlite3 *db, char *zErrMsg, int name) {
+	int rc;
+	sqlite3_stmt *del_record;
+
+	rc = sqlite3_prepare_v2(db, "DELETE FROM CONTACTS WHERE ID = ?;", -1, &del_record, NULL);
+	if ( rc != SQLITE_OK ) {
+		fprintf(stderr, "SQL Error Prepare failed: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+	sqlite3_bind_int(del_record, 1, name);
+
+	for (;;) {
+		rc = sqlite3_step(del_record);
+		if (rc != SQLITE_ROW)
+			break;	
+	}
+
+	if ( rc != SQLITE_DONE ) {
+		fprintf(stderr, "SQL Execution failed: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(del_record);
+		return;
+	}
+
+	sqlite3_finalize(del_record);
+	return;
+}
+
 void find_record(sqlite3 *db, char *zErrMsg, char *query) {
 	int rc;
 	sqlite3_stmt *find_record;
 
-	sqlite3_prepare_v2(db, "SELECT * FROM CONTACTS WHERE NAME LIKE '?%';", 200, &find_record, NULL);
-	if ( find_record != NULL ) {
-		sqlite3_bind_text(find_record, 1, query, -1, NULL);
-		int step = sqlite3_step(find_record);
+	rc = sqlite3_prepare_v2(db, "SELECT * FROM CONTACTS WHERE NAME = ?;", -1, &find_record, NULL);
+	if ( rc != SQLITE_OK ) {
+		fprintf(stderr, "SQL Error Prepare failed: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+	sqlite3_bind_text(find_record, 1, query, strlen(query), SQLITE_TRANSIENT);
 
-		if (step == SQLITE_ROW) {
-			printf("ID = %d\nNAME = %s\nADDRESS = %s\nPHONENUMBER = %s\n", sqlite3_column_int(find_record, 0), sqlite3_column_text(find_record, 1), sqlite3_column_text(find_record, 2), sqlite3_column_text(find_record, 3));
-		}
-
-		rc = sqlite3_finalize(find_record);
+	for (;;) {
+		rc = sqlite3_step(find_record);
+		if (rc != SQLITE_ROW)
+			break;
+		
+		int id = sqlite3_column_int (find_record, 0);
+		const char *name = sqlite3_column_text(find_record, 1);
+		const char *address = sqlite3_column_text(find_record, 2);
+		const char *phonenumber = sqlite3_column_text(find_record, 3);
+		if (name == NULL)
+			name = "(NULL)";
+		if (address == NULL)
+			address = "(NULL)";
+		if (phonenumber == NULL)
+			phonenumber = "(NULL)";
+		printf("\nID = %d\nNAME = %s\nADDRESS = %s\nPHONENUMBER = %s\n\n", id, name, address, phonenumber);
 	}
 
-	if ( rc != SQLITE_OK ){
-		fprintf(stderr, "SQL Error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	} else {
-		fprintf(stdout, "Record retrieved with success.\n");
+	if ( rc != SQLITE_DONE ) {
+		fprintf(stderr, "SQL Execution failed: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(find_record);
+		return;
 	}
+
+	sqlite3_finalize(find_record);
+	return;
 }
 
 void menu(sqlite3 *db, char *zErrMsg) {
@@ -126,8 +194,26 @@ void menu(sqlite3 *db, char *zErrMsg) {
 				scanf("%s", &name);
 				find_record(db, zErrMsg, name); 
 				break;
+			case 3:
+				int name_id;
+				printf("Enter the ID of contact to delete: ");
+				scanf("%d", &name_id);
+				delete_record(db, zErrMsg, name_id);
+				select_all_records(db, zErrMsg);
+				break;
+			case 4:
+				contact new_c;
+				printf("Enter the NAME of the new contact: ");
+				scanf("%s", &new_c.name);
+				printf("Enter the ADDRESS of the new contact: ");
+				scanf("%s", &new_c.addr);
+				printf("Enter the PHONENUMBER of the new contact: ");
+				scanf("%s", &new_c.phone);
+				insert_records(db, zErrMsg, new_c);
+				select_all_records(db, zErrMsg);
+				break;
 			default :
-				printf("Exiting ...");
+				printf("Exiting ...\n");
 				return;
 				break;
 		}
@@ -152,7 +238,13 @@ int main(int argc, char* argv[]) {
 
 	if ( !db_existed ) {	
 		create_tables(db, zErrMsg);
-		insert_records(db, zErrMsg);
+
+		contact c = {
+			"3n3a",
+			"Street 1",
+			"0791234567",
+		};
+		insert_records(db, zErrMsg, c);
 	}
 
 	menu(db, zErrMsg);
